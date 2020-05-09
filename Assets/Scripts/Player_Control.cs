2a8765e69f54c4e2ee5f3ffa22072330a7002fa5
mehonosh <mehonosh.nikitos@gmail.com>
2020-05-09 19:18:06 +0300

    Первая проба

---
+++ Assets/Scripts/Player_Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(-ver, 0, hor);
+        transform.Translate(dir.normalized * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/Spawn_object.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_object : MonoBehaviour
+{
+    public GameObject[] obj; // Объявление массива обьектов
+    public float[] positions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(
+                 obj[Random.Range(0, obj.Length)],
+                 new Vector3(Random.Range(-23, 23), 0, Random.Range(-23, 23)),
+                 Quaternion.Euler(new Vector3(0, -180, 0))
+                 );
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
