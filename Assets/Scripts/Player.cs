c056f0c2e6e4e0c809a291af25b79b3f52ae2101
mehonosh <mehonosh.nikitos@gmail.com>
2020-05-09 20:04:43 +0300

    Добавление коробки и уничтожение объекта

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        // машинка уничтожается при столкновении
+        if (collision.gameObject.CompareTag("Obj"))
+        {
+
+            Destroy(gameObject); //уничтожается объект
+             // включается камера, где написано что объект проиграл
+            //Time.timeScale = 0;
+            //isPaused = true;
+        }
+    }
+
+  /*  private void OnTriggerEnter(Collider collision)
+    {
+        //CoinCollect.coinCount += 1;
+        Destroy(collision.gameObject);
+    }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
