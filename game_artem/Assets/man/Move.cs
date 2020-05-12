964b63a5fcc8900cf1f1576e163760625e32b4bb
Artemsh <petrovtvr95@yandex.ru>
2020-05-12 21:54:47 +0300

    добавил человека и анимацию (w,s)

---
+++ game_artem/Assets/man/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+public class move : MonoBehaviour
+{
+    public GameObject player;
+    public int speed = 5;
+    public Animator anim;
+
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+        anim =  GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+            anim.enabled = true;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ game_artem/Assets/man/scpirts/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour
+{
+    public int speed = 5;
+    public Animator anim;
+
+    void Start()
+    {
+        anim =  GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //движение вперед + включение анимации
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+            //anim.enabled = true;
+            anim.enabled = !anim.enabled;
+        }
+        //движение назад + включение анимации
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            anim.enabled = !anim.enabled;
+        }
+    }
+}
